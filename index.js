1efaae89f7d6fe95b9a7ebd119ae8ba62b2e539d
Lortas102066 <mitsukimorinaga@gmail.com>
2024-07-01 17:24:11 +1000

    Praciticing getting used to node js

---
+++ index.js
@@ -0,0 +1,5 @@
+const Person = require("./person");
+
+const person1 = new Person("Mitsuki", 20);
+
+person1.greeting();
---
+++ reference/os.js
@@ -0,0 +1,19 @@
+const os = require('os');
+
+// Platform
+console.log(os.platform());
+
+// CPU Architecture
+console.log(os.arch());
+
+// CPU Core Info
+console.log(os.cpus());
+
+// Free Memory
+console.log(os.freemem());
+
+// Home dir
+console.log(os.homedir());
+
+// uptime
+console.log(os.uptime());
