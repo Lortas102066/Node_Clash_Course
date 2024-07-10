bccbe9c320c7fe7e69b42a7decf83d11b1258e1e
Lortas102066 <mitsukimorinaga@gmail.com>
2024-07-10 13:23:45 +1000

    aa

--- index.js
+++ index.js
@@ -1,81 +1,28 @@
-const http = require('http');
-const path = require('path');
-const fs = require('fs');
+import express from "express";
 
-const server = http.createServer((req, res) => {
-    // if (req.url === '/') {
-    //     fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
-    //         if (err) throw err;
-    //         res.writeHead(200, {'Content-Type': 'text/html'});
-    //         res.end(content);
-    //     });
-    // } else if (req.url === '/about') { 
-    //     fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
-    //         if (err) throw err;
-    //         res.writeHead(200, {'Content-Type': 'text/html'});
-    //         res.end(content);
-    //     });
-    // }
-    
-    // if (req.url === '/api/users') {
-    //     const users = [
-    //         {name : 'John', age: 20},
-    //         {name : 'Money', age: 25}
-    //     ]
-    //     res.writeHead(200, {'Content-Type': 'application/json'});
-    //     res.end(JSON.stringify(users));
-    // } 
+const app = express();
+const port = 3000;
+app.use(express.static("public"));
 
-    // Build file path
-    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+/* Write your code here:
+Step 1: Render the home page "/" index.ejs
+Step 2: Make sure that static files are linked to and the CSS shows up.
+Step 3: Add the routes to handle the render of the about and contact pages.
+  Hint: Check the nav bar in the header.ejs to see the button hrefs
+Step 4: Add the partials to the about and contact pages to show the header and footer on those pages. */
 
-    // Extension of file
-    let extname = path.extname(filePath);
-
-    // Initial Content Type
-    let contentType = 'text/html';
-
-    //Check ext and set content type
-    switch (extname) {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;
-        case '.jpg':
-            contentType = 'image/jpg';
-            break;
-    }
-
-    //Read File
-    fs.readFile(filePath, (err, content) => {
-        if (err) {
-            if(err.code == 'ENOENT') {
-                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
-                    res.writeHead(200, {'Content-Type': 'text/html'});
-                    res.end(content, 'utf-8');
-                })
-            } else {
-                //Some server error
-                res.writeHead(500);
-                res.end('Server error: ' + err);
-            }
-        } else {
-            res.writeHead(200, {'Content-Type': contentType});
-            res.end(content, 'utf-8');
-        }
-    })
+app.get('/', (req, res) => {
+  res.render('index.ejs');
 });
 
-const PORT = process.env.PORT || 5001;
+app.get('/contact', (req, res) => {
+  res.render('contact.ejs');
+});
 
-server.listen(PORT, function () {
-    console.log('Server Running on port ' + PORT);
+app.get('/about', (req, res) => {
+  res.render('about.ejs');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
